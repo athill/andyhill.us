16b08363e340156587d46fbbbbdbd872344c586b
Andy Hill <andy@andyhill.us>
2013-03-16 10:42:57 -0400

    d3: added simple geekout to index.php

--- d3/index.php
+++ d3/index.php
@@ -2,6 +2,16 @@
 $local['jsModules']['d3'] = true;
 $local['scripts'] = array('index.js');
 include('../inc/application.php');
+$h->startBuffer();
+$g3=<<<EOT
+<a href="http://d3js.org/" target="_blank">D3.js</a> is a JavaScript library for manipulating 
+documents based on data. Most of what I've done is just tutorials and simple things, but I'd 
+like to do more things like <a href="${site['webroot']}/d3/line.php">this</a>.
+EOT;
+$h->p($g3);
+
+$geekout = $h->endBuffer();
+$template->template->geekOut($geekout);
 //print_r($GLOBALS['site']);
 ?>
 
