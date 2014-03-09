196edf00a85c67f22769c60b282839ba77f50633
Andy Hill <andy@andyhill.us>
2014-03-09 18:13:58 -0400

    informed electorate: synced up setup.inc.php with prd

--- informedelectorate.net/inc/setup.inc.php
+++ informedelectorate.net/inc/setup.inc.php
@@ -5,7 +5,7 @@ date_default_timezone_set('America/New_York');
 
 ////Discover webroot and fileroot
 $webroot = "";
-$basefileroot = "/home/andyhil/public_html";
+$basefileroot = "/home/andyhil/public_html/informedelectorate.net";
 ////dev environments
 if (stripos($_SERVER['SCRIPT_NAME'], "/new/") === 0) {
 	$webroot .= "/new";
