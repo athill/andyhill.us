696e63c5962bae7c39184695258ddd360aa4c388
Andy Hill <andy@andyhill.us>
2015-05-12 17:30:50 -0600

    removed old

--- old/computers/javachat/server2.java
+++
@@ -1,5 +0,0 @@
-class Server2 {
-    public static void main(String[] args) {
-        new QuoteServerThread().start();
-    }
-}
--- old/family/text.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-$alt = preg_replace("/<[^>]*>/", "", "This is my dad, <a href='motioncontrol/motioncontrolhome.html'>Troy Hill</a>, and me outside my 
-house.");  
-print("$alt");
-
-?>
