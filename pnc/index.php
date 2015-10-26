efaa20d5cde8d940f447039c9d56c7af5e0de605
Andy Hill <andy@andyhill.us>
2015-10-26 13:41:27 -0400

    pnc: format form

--- pnc/index.php
+++ pnc/index.php
@@ -8,31 +8,26 @@
 <body>
 <h1>PNC Response</h1>
 
-<form action="submit.php" method="post">
-Name: <input typr="text" name="name"/><br>
-Email: <input typr="text" name="email"/><br>
-Type: <select name="type">
-	<option>Match</option>
-	<option>Referral</option>
-</select><br />
-
-<div class="form-group">
-	<label for="name">Name</label>
-	<input type="text" class="form-control" id="name" placeholder="Name">
+<div class="container">
+	<form action="submit.php" method="post">
+	<div class="form-group">
+		<label for="name">Name</label>
+		<input type="text" class="form-control" id="name" placeholder="Name">
+	</div>
+	<div class="form-group">
+		<label for="email">Email</label>
+		<input type="email" class="form-control" id="email" placeholder="Email">
+	</div>
+	<div class="form-group">
+		<label for="type">Type</label>
+		<select class="form-control" id="type">
+			<option>Match</option>
+			<option>Referral</option>
+		</select>
+	</div>
+	<button type="submit" class="btn btn-default">Send Mail</button>
+	</form>
 </div>
-<div class="form-group">
-	<label for="email">Email</label>
-	<input type="email" class="form-control" id="email" placeholder="Email">
-</div>
-<div class="form-group">
-	<label for="type">Type</label>
-	<select class="form-control" id="type">
-		<option>Match</option>
-		<option>Referral</option>
-	</select>
-</div>
-<button type="submit" class="btn btn-default">Send Mail</button>
-</form>
 
 </body>
 </html>
