0665b1e84c596c140598a69734082645bd977993
Andy Hill <andy@andyhill.us>
2015-10-26 10:22:12 -0400

    pnc: debugging 3

--- pnc/index.php
+++ pnc/index.php
@@ -7,7 +7,7 @@
 <body>
 <h1>PNC Response</h1>
 
-<form action="submit.php">
+<form action="submit.php" method="post">
 Name: <input typr="text" name="name"/><br>
 Email: <input typr="text" name="email"/><br>
 Type: <select name="type">
