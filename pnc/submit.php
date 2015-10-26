847f0f6154dd01fb054630d3c80fc2c6dfd1aecf
Andy Hill <andy@andyhill.us>
2015-10-26 11:37:12 -0400

    pnc: clean up , attaching mif

--- pnc/submit.php
+++ pnc/submit.php
@@ -1,46 +1,36 @@
 <?php
 require('../vendor/autoload.php');
 
-echo 'here';
-
 $to = $_POST['email'];
 
 $subject = 'Website Change Request';
 
-$headers = "From: andy@andyhill.us\r\n";
-$headers .= "MIME-Version: 1.0\r\n";
-$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
-
+//// Build message
 $templatefile = 'templates/'.strtolower($_POST['type']).'.html';
-
-echo 'file: '.$templatefile;
-
-$template = file_get_contents($templatefile);
-$template = str_replace('[Name]', $_POST['name'], $template);
-
-$message = $template;
-// $message .= '<h1>Hello, World!</h1>';
+$message = file_get_contents($templatefile);
+$message = str_replace('[Name]', $_POST['name'], $message);
 
 
-echo '<div>'.$message.'</div>';
+//// Build email
+$mail = new PHPMailer();
+$mail->From      = 'andy@andyhill.us';
+$mail->FromName  = 'Andy Hill';
+$mail->Subject   = 'Response';
+$mail->Body      = $message;
+$mail->AddAddress($_POST['email']);
+$mail->isHTML(true);      
 
-// mail($to, $subject, $message, $headers);
+if ($_POST['type'] == 'Match') {
+	$mail->addAttachment('ministry_information_form_2015.doc', 'UPC Ministry Information Form, 2015');    // Optional name	
+}
 
-$email = new PHPMailer();
-$email->From      = 'andy@andyhill.us';
-$email->FromName  = 'Andy Hill';
-$email->Subject   = 'Response';
-$email->Body      = $message;
-$email->AddAddress($_POST['email']);
-
-
-echo 'SENT';
-// $file_to_attach = 'PATH_OF_YOUR_FILE_HERE';
-
-$return = $email->Send();
+//// send email
+if(!$mail->send()) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+    print('Sent: <div>'.$message.'</div> to: '.$_POST['email'].' ('.$_POST['email'].')';
+}
 
 
-var_dump($return);
-
-// $email->AddAttachment( $file_to_attach , 'NameOfFile.pdf' );
