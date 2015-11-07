569b4e188ba5c2708a264b645bdaf0d58d69a9a7
Andy Hill <andy@andyhill.us>
2015-11-07 10:27:07 -0500

    pnc: tweaking file upload

--- pnc/submit.php
+++ pnc/submit.php
@@ -7,18 +7,19 @@ $data = $_POST;
 require('PastorEmail.php');
 $pastorEmailer = new PastorEmailer();
 
-if (isset($data['data-file'])) {
+if (isset($_FILES['data-file'])) {
+	$dataFile = $_FILES['data-file'];
 	//// TODO: upload file
 	$target_dir = "uploads/";
 	if (!file_exists($target_dir)) {
 		mkdir($target_dir);
 	}
-	$target_path = $target_dir . basename( $_FILES['uploadedfile']['name']); 
+	$target_path = $target_dir . basename($dataFile['name']); 
 
-	if(move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $target_path)) {
-	    echo "The file ".  basename( $_FILES['uploadedfile']['name']). 
+	if (move_uploaded_file($dataFile['tmp_name'], $target_path)) {
+	    echo "The file ".  basename($dataFile['name']). 
 	    " has been uploaded";
-	} else{
+	} else {
 	    echo "There was an error uploading the file, please try again!";
 	}
 	unlink($target_path);
