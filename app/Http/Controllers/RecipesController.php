ee156fc7cec713298fd65907b5b912e8feb53ed3
Andy Hill <andy@andyhill.us>
2017-12-01 19:42:20 -0500

    RecipesController: getting appropriate json. woot

--- app/Http/Controllers/RecipesController.php
+++ app/Http/Controllers/RecipesController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-use File;
+// use File;
 
 class RecipesController extends Controller
 {
+    private $xml;
+
     /**
      * Display a listing of the resource.
      *
@@ -15,9 +17,50 @@ class RecipesController extends Controller
      */
     public function index()
     {
-        $recipesxml = File::get(storage_path('data/recipes/recipes.xml'));
-        dd($recipesxml);
-        return ['foo' => 'bar'];
+        if (!$this->xml = simplexml_load_file(storage_path('data/recipes/recipes.xml'))){
+           trigger_error('Error reading XML file',E_USER_ERROR);
+        }
+       return $this->getJsonFromXml();
+    }
+
+    private function getJsonFromXml() : array {
+        $recipes = [];
+        foreach ($this->xml as $recipe) {
+            $json = [
+                'id' => (string) $recipe['id'],
+                'instructions' => explode("\n", (string) $recipe->instructions)
+            ];
+            if ($recipe->modifications && strlen(trim((string) $recipe->modifications)) > 0) {
+                $json['notes'] = explode("\n", (string) $recipe->modifications);
+            }
+            //// simple tags
+            $tagnames = ['title', 'category','cuisine','rating','preptime','servings','cooktime'];
+            foreach ($tagnames as $tagname) {
+                $json[$tagname] = (string)$recipe->{$tagname};
+            }
+            //// instructions
+            $ingredients = [];
+            foreach ($recipe->{'ingredient-list'}->ingredient as $ingredient) {
+                $ingredients[] = [
+                    'amount' => (string) $ingredient->amount,
+                    'unit' => (string) $ingredient->unit,
+                    'item' => (string) $ingredient->item,
+                    'key' => (string) $ingredient->key
+                ];
+            }
+            $ingredients = [];
+            foreach ($recipe->{'ingredient-list'}->ingredient as $ingredient) {
+                $ingredients[] = [
+                        'amount' => (string) $ingredient->amount,
+                        'unit' => (string) $ingredient->unit,
+                        'item' => (string) $ingredient->item,
+                        'key' => (string) $ingredient->key
+                ];
+            }
+            $json['ingredients'] = $ingredients;
+            $recipes[] = $json;
+        }
+        return $recipes;
     }
 
  
