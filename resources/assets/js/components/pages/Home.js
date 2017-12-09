df8b75d0dfb8b9c3f15d248d9ccd9faf627b51d7
Andy Hill <andy@andyhill.us>
2017-12-09 12:21:49 -0500

    Updated home page

--- resources/assets/js/components/pages/Home.js
+++ resources/assets/js/components/pages/Home.js
@@ -3,26 +3,23 @@ import React from 'react';
 const Home = () => (
 	<div>
 		<p>
-			My name is Andy Hill. I am a programmer in <a href="http://bloomington.in.gov/" target="_blank">Bloomington, Indiana</a>. &nbsp;
+			My name is Andy Hill. I am a programmer in <a href="http://bloomington.in.gov/" target="_blank" rel="noopener">Bloomington, Indiana</a>. &nbsp;
 			Primarily a web developer. I got my Bachelor of Science in Computer Science from &nbsp;
-			<a href="https://www.iu.edu/" target="_blank">Indiana University</a> &nbsp;
-			at the age of 33. I currently work for the University in <a href="https://uits.iu.edu/" target="_blank"> &nbsp;
+			<a href="https://www.iu.edu/" target="_blank" rel="noopener">Indiana University</a> I currently work at IU in&nbsp; 
+			<a href="https://uits.iu.edu/" target="_blank" rel="noopener"> &nbsp;
 			<abbr title="University Information Technology Services">UITS</abbr></a>&apos;s &nbsp;
 			<abbr title="Enterprise Service Integration">ESI</abbr> Middleware Team. There is no relationship between Indiana University and this site.
 		</p>
 		<p>
-			//// Fix This
-			This site was originally a playground for me to play with web programming technologies, though I tend to use other sites for that now. 
-			though there is value to the content as well (I especially find the 
-			<a href="/news/">News</a> page practical). Many pages will have a "Geek Out" button 
-			at the top. Clicking on this will 
-			toggle technical information about the technologies used on the page.
+			This site was originally a playground for me to play with web programming technologies, though I tend to use&nbsp;  
+			<a href="https://informedelectorate.net/" target="_blank" rel="noopener" >other</a>&nbsp;  
+			<a href="https://wimf.space/" target="_blank" rel="noopener">sites</a> for that now. 
 		</p>
 		<p>I've been developing this site intermittently since 2003. Here are a few snapshots through the years (note, not everything works on these sites, but you should be to get around)</p>
 		<ul>
-			<li><a href="http://old.andyhill.us/" target="_blank">2003-2006</a></li>
-			<li><a href="http://2-0.andyhill.us/" target="_blank">2007-2011</a></li>
-			<li><a href="http://3-0.andyhill.us/" target="_blank">2012-2016</a></li>
+			<li><a href="http://old.andyhill.us/" target="_blank" rel="noopener">2003-2006</a></li>
+			<li><a href="http://2-0.andyhill.us/" target="_blank" rel="noopener">2007-2011</a></li>
+			<li><a href="http://3-0.andyhill.us/" target="_blank" rel="noopener">2012-2017</a></li>
 		</ul>
 	</div>
 
