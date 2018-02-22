b178ca429709cb6e83304234c0090318b4002c78
Andy Hill <andy@andyhill.us>
2018-02-22 17:37:03 -0500

    inspire: update controller to use service

--- app/Http/Controllers/InspireController.php
+++ app/Http/Controllers/InspireController.php
@@ -8,33 +8,18 @@ use Cache;
 use File;
 use Log;
 
+use App\Services\InspireService;
+
 class InspireController extends Controller {
 	private $data;
 
     public function index() {
-    	$this->data =  Cache::remember('inspire', 60, function () {
-    		return $this->getData();
-    	});
-        return $this->data;
+    	$cachekey = InspireService::CACHE_KEY;
+    	return Cache::get($cachekey);
+    	//// get data directly
+    	// $service = new InspireService;
+    	// return $service->getData();
     }
 
-    private function getData() : array {
-    	$files = File::allFiles(storage_path('data/inspire'));
-    	// dd($files);
-    	$data = [];
-		foreach ($files as $file) {
-			$path = $file->getPath();
-			$area = preg_replace("/.*\/([^\/]+)$/", "$1", $path); 
-		    if (!isset($data[$area])) {
-		    	$data[$area] = [];
-		    }
-		    $key = preg_replace("/(.*)\.txt/", "$1", $file->getBasename());
-		    $content = file_get_contents($file->getPathname());
-		    list($title, $content) = explode("\nCONTENT\n", $content);
-		    $sections = explode("\nCREDITS\n", $content);
-		    list($content, $credits) = $sections;
-		    $data[$area][] = [ 'key' => $key, 'content' => $content, 'credits' => $credits, 'title' => $title ];
-		}
-		return $data;
-    }
+
 }
