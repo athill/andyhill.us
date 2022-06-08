caa24c694a9dee68e7ec06b7771d79b8991fd63d
Andy Hill <athill@iu.edu>
2022-06-08 19:22:17 -0400

    express.js

--- src/js/components/pages/Home.js
+++ src/js/components/pages/Home.js
@@ -5,22 +5,22 @@ const Home = () => (
 		<p>
 			My name is Andy Hill. I am a programmer in <a href="http://bloomington.in.gov/" target="_blank" rel="noreferrer">Bloomington, Indiana</a>. &nbsp;
 			Primarily a web developer. I got my Bachelor of Science in Computer Science from &nbsp;
-			<a href="https://www.iu.edu/" target="_blank" rel="noreferrer">Indiana University</a> I currently work at IU in&nbsp; 
+			<a href="https://www.iu.edu/" target="_blank" rel="noreferrer">Indiana University</a> I currently work at IU in&nbsp;
 			<a href="https://uits.iu.edu/" target="_blank" rel="noreferrer"> &nbsp;
 			<abbr title="University Information Technology Services">UITS</abbr></a>&apos;s &nbsp;
-			<abbr title="Enterprise Service Integration">ESI</abbr> Middleware Team. There is no relationship between Indiana University and this site.
+			Enterprise Platforms Team. There is no relationship between Indiana University and this site.
 		</p>
-		<p>
-			This site was originally a playground for me to play with web programming technologies, though I tend to use&nbsp;  
-			<a href="https://informedelectorate.net/" target="_blank" rel="noreferrer" >other</a>&nbsp;  
-			<a href="https://wimf.space/" target="_blank" rel="noreferrer">sites</a> for that now. 
+		{/* <p>
+			This site was originally a playground for me to play with web programming technologies, though I tend to use&nbsp;
+			<a href="https://informedelectorate.net/" target="_blank" rel="noreferrer" >other</a>&nbsp;
+			<a href="https://wimf.space/" target="_blank" rel="noreferrer">sites</a> for that now.
 		</p>
 		<p>I've been developing this site intermittently since 2003. Here are a few snapshots through the years (note, not everything works on these sites, but you should be to get around)</p>
 		<ul>
 			<li><a href="http://old.andyhill.us/" target="_blank" rel="noreferrer">2003-2006</a></li>
 			<li><a href="http://2-0.andyhill.us/" target="_blank" rel="noreferrer">2007-2011</a></li>
 			<li><a href="http://3-0.andyhill.us/" target="_blank" rel="noreferrer">2012-2017</a></li>
-		</ul>
+		</ul> */}
 	</div>
 
 );
